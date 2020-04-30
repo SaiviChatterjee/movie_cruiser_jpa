d092c8b408d31321127a162342ce3d740de1637e
SaiviChatterjee <40817236+SaiviChatterjee@users.noreply.github.com>
2020-04-30 06:49:51 +0530

    Update MovieService.java

--- moviecruiser-jpa/src/main/java/com/cognizant/moviecruiser/service/MovieService.java
+++ moviecruiser-jpa/src/main/java/com/cognizant/moviecruiser/service/MovieService.java
@@ -2,6 +2,8 @@ package com.cognizant.moviecruiser.service;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,22 +15,27 @@ public class MovieService {
 	@Autowired
 	private MovieRepository movieRepository;
 	
+	@Transactional
 	public Iterable<Movie> addMovies(List<Movie> movies) {
 		return movieRepository.saveAll(movies);
 	}
 
+	@Transactional
 	public Iterable<Movie> getMoviesAdmin() {
 		return movieRepository.findAll();
 	}
 
+	@Transactional
 	public Iterable<Movie> getMoviesCustomer() {
 		return movieRepository.findMoviesCustomer();
 	}
-
+	
+	@Transactional
 	public Movie getMovieById(Long movieId) {
 		return movieRepository.findById(movieId).get();
 	}
 
+	@Transactional
 	public Movie updateMovie(Movie movie) {
 		return movieRepository.save(movie);
 	}
